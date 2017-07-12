9dc5b13873d1391a50a944f2e4d530827ed106d8
AdamLoe <afiddlincracker@gmail.com>
2017-07-12 12:57:42 -0500

    automated commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var routes = require('./routes');
 
 app.get('/', function (req,res) {
     console.log('Hey i did a thing.');
-    res.send( 'HiYa there5');
+    res.send( 'HiYa there6');
 });
 console.log('Now Serving /');
 
