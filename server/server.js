a23fed4d8117ea93b84577d3d55976733ff53e59
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-07-16 17:15:23 -0500

    lkj

--- server/server.js
+++ server/server.js
@@ -1,58 +1,33 @@
-/**
- * Created by Adam on 7/11/2017.
- */
-var pg = require('knex')({
-    client: 'pg',
-    searchPath: 'knex,public',
-    debug: true,
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'postgres'
-    }
-});
-
-
 var express = require('express');
 var app = express();
 
-var path = require('path');
+var home    = require('./routes/home');
+var admin   = require('./routes/admin');
+var profile = require('./routes/profile');
+var login   = require('./routes/login');
 
-var helpers = require('./helpers');
-var routes = require('./routes');
-
-
+//Since we're serving app on cdn (github pages)
 var cors = require('cors');
-
 app.use(cors());
 
-app.get('/', function (req,res) {
-    console.log('Hey i did a thing.');
-    pg.select().from('adam').timeout(1000);
-    res.send( 'HiYa there8');
-});
-console.log('Now Serving /');
 
-app.get('/home*', function (req,res) {
-    helpers.catchErrors( res.send( 'Home Page, aka your request feed.') );
-});
+var isLoggedIn = false;
+/*
+app.use(function(req, res, next) {
 
-app.get('/profile/*', function (req,res) {
-    res.send( 'Look at your or other profiles');
+    next();
 });
-
-app.get('/register', function (req,res) {
-    res.send( 'Register Here');
-});
-
-app.get('/history/*', function (req,res) {
-    res.send( 'Look up history of users here');
-});
-
-app.get('/admin', function (req,res) {
-    res.send( 'Admin Panel Here');
-});
+*/
+if (isLoggedIn === true) {
+    console.log('A logged in user just requested site');
+    app.use('/home*', home);
+    app.use('/admin*', admin);
+    app.use('/profile*', profile);
+}
+else {
+    console.log('A non logged in user accessed site');
+    app.use('/login', login);
+}
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use( function(req, res, next) {
@@ -72,14 +47,6 @@ app.use( function(err, req, res, next) {
     res.redirect('back');
 });
 
-
-
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-console.log('Now serving /public');
-
 app.listen( process.env.PORT || 7778 );
-
 console.log('Now Serving', process.env.PORT || 7777 );
+
