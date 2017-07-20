c899c6505855132e384fe07d241d250394e36979
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-07-20 01:13:10 -0500

    asdf

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 
+/*
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
+*/
 
 var express = require('express');
 var app = express();
@@ -39,15 +41,14 @@ app.use( function(err, req, res, next) {
 var cors = require('cors');
 app.use(cors());
 
+app.listen(80);
+
+/*
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80);
 httpsServer.listen(443);
-
-//Since we're serving app on cdn (github pages)
-
-/*
 
  var passport = require('passport');
  var LocalStrategy = require('passport-local').Strategy;
