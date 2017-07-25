8f9fe412c5aafb92a3c6e0880cd5f3279423dd9d
AdamLoe <afiddlincracker@gmail.com>
2017-07-25 07:07:32 -0500

    automated commit

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use((req, res, next) => {
     if (req.secure) {
         next();
     } else {
-        res.redirect('https://' + req.hostname + req.url);
+        res.redirect('https://standardrequests.com/' + req.url);
 }});
 
 //MiddleWare
