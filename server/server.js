6ed99d7b59d736a3dd014a7a28d146df5d034d0a
AdamLoe <afiddlincracker@gmail.com>
2017-07-25 07:32:29 -0500

    automated commit

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ var routes = require('./routes');
 app.use('/api/*', routes);
 //Otherwise, just give them the client webApp
 app.use('*', function(req,res){
-    res.sendFile('/home/adam/standard-requests/public/dist/index.html');
+    res.sendFile('/home/adam/standard-requests/dist/index.html');
 });
 
 
