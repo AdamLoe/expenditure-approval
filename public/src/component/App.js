a338ec7f1703a19bdc75635f649b4c3f16aa2746
AdamLoe <afiddlincracker@gmail.com>
2017-07-26 08:10:13 -0500

    automated commit

--- public/src/component/App.js
+++ public/src/component/App.js
@@ -1,12 +1,10 @@
 var React = require('react');
 var ReactRouter = require('react-router-dom');
 
-var browserHistory = ReactRouter.browserHistory;
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
 
-var NavBar = require('./NavBar');
 var Home = require('./Home');
 var Login = require('./Login');
 var Admin = require('./Admin');
@@ -20,13 +18,12 @@ class App extends React.Component {
 
     render() {
         return (
-            <Router history={browserHistory}>
+            <Router>
                 <div className='App'>
-                    <NavBar />
                     <Switch>
                         <Route exact path='/'        component={Home}       />
                         <Route path='/home'          component={Home}  />
-                        <Route path='/login'         component={MyProfile}  />
+                        <Route path='/login'         component={Login}  />
                         <Route path='/admin'         component={Admin}      />
                         <Route render={function() {return<p>Not Found</p>}} />
                     </Switch>
