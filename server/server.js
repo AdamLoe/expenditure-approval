0099ddf0b1248238335ddbc57a7623bc78bb0027
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-07-28 12:55:47 -0500

    asdf

--- server/server.js
+++ server/server.js
@@ -13,13 +13,7 @@ app.use( function (req, res, next) {
     if (req.secure) {
         next();
     } else {
-        if (NODE_ENV === 'production') {
             res.redirect('https://standardrequests.com' + req.url);
-        } else {
-            next();
-        }
-        /*
-        */
     }
 });
 
@@ -39,12 +33,10 @@ app.use('*', function(req,res){
 var http  = require('http');
 http.createServer(app).listen(80);
 
-if (NODE_ENV === 'production') {
-    var fs = require('fs');
-    var privateKey = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/privkey.pem', 'utf8');
-    var certificate = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/cert.pem', 'utf8');
-    var ca = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/chain.pem', 'utf8');
-    var options = {key: privateKey, cert: certificate, ca: ca};
-    var https = require('https');
-    https.createServer(options, app).listen(443);
-}
+var fs = require('fs');
+var privateKey = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/cert.pem', 'utf8');
+var ca = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/chain.pem', 'utf8');
+var options = {key: privateKey, cert: certificate, ca: ca};
+var https = require('https');
+https.createServer(options, app).listen(443);
