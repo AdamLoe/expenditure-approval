411ab3bda6cf5238198987d4d42cf3aba856f789
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-07-30 03:54:17 -0500

    asdf

--- SQL Scripts/fillTables.sql
+++ SQL Scripts/fillTables.sql
@@ -1,16 +1,75 @@
-{
-    requestId: 1107, requestName:'HVAC',
-    requesterName: 'John Liberty', requesterId: 17,
-    nextApproverName: 'Hannah Valentine', nextApproverId: 13,
-    status: 'In Process',  amount: 1900.00,
-    propertyName: 'Signature Place', propertyId: 103,
-    unitName: '81-081',  itemType: 'Financial',
-    requestorComment: '', createDate: '', lastEditDate: '',
-    attributes: [
-        ['Price', '2524'],['Shipping', '0'],
-        ['Tax', '0'], ['OtherCost', '0']
-    ],
-    comments: [
-        ['Date, text, user']
-    ]
-},
+INSERT INTO users VALUES (
+ -- username  |  password
+    'eddyG'   ,   'earl'
+ --   name    |    type
+'Eddy Gorreat',  'approver'
+ --  nextAprv |  aprvLimit
+      -1      ,    -1
+)
+INSERT INTO users VALUES (
+ -- username  |  password
+    'sarahP'  ,   'sarah'
+ --   name    |    type
+'Sarah Parantino',  'approver'
+ --  nextAprv |  aprvLimit
+      'eddyG' ,    5000
+)
+INSERT INTO users VALUES (
+ -- username  |  password
+    'sandraT'  ,   'sandra'
+ --   name    |    type
+'Sandra Tornapotarainyapolis' ,  'approver'
+ --  nextAprv |  aprvLimit
+      'eddyG'  ,    4000
+)
+INSERT INTO users VALUES (
+  -- username  |  password
+     'tonyaB'  ,   'tonya'
+  --   name    |    type
+ 'Tonya Baker' ,  'approver'
+  --  nextAprv |  aprvLimit
+       'sarahP'  ,    3000
+ )
+ INSERT INTO users VALUES (
+  -- username  |  password
+     'gerryL'  ,   'gerry'
+  --   name    |    type
+ 'Gerry Lawnrence' ,  'approver'
+  --  nextAprv |  aprvLimit
+       'sandraT'  ,   1000
+ )
+INSERT INTO users VALUES (
+  -- username  |  password
+     'crawfordPlace'  ,   'crawford'
+  --   name    |    type
+ 'Crawford Place' ,  'requester'
+  --  nextAprv |  aprvLimit
+       'tonyaB'  ,   0
+ )
+ INSERT INTO users VALUES (
+   -- username  |  password
+      'corneliusVilla'  ,   'cornelius'
+   --   name    |    type
+  'Cornelius Villa Apartments' ,  'requester'
+   --  nextAprv |  aprvLimit
+        'tonyaB'  ,   0
+  )
+INSERT INTO users VALUES (
+  -- username  |  password
+     'debianHomes'  ,   'debian'
+  --   name    |    type
+ 'Debian Homes' ,  'requester'
+  --  nextAprv |  aprvLimit
+       'sandraT'  ,   0
+ )
+INSERT INTO users VALUES (
+  -- username  |  password
+     'marklandApts'  ,   'markland'
+  --   name    |    type
+ 'Markland Apts' ,  'requester'
+  --  nextAprv |  aprvLimit
+       'gerryL'  ,   0
+ )
+
+
+
