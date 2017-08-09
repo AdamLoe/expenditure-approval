2f920449843fb51c034c1684d7650970edabf796
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-08-08 23:28:34 -0500

    asdf

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ app.use( function (req, res, next) {
     if (req.secure) {
         next();
     } else {
+        next();
+        /*
         res.redirect('https://standardrequests.com' + req.url);
+        */
     }
 });
 
@@ -29,16 +32,18 @@ app.use('/api/', routes);
 
 //Otherwise, just give them the client webApp
 app.use('*', function(req,res){
-     res.sendFile('/home/adam/standard-requests/public/dist/index.html');
+     res.sendFile('/Github/standard-requests/public/test/login/login.html');
 });
 
 
 var http  = require('http');
 http.createServer(app).listen(80);
-var fs = require('fs');
-var privateKey = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/privkey.pem', 'utf8');
-var certificate = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/cert.pem', 'utf8');
-var ca = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/chain.pem', 'utf8');
-var options = {key: privateKey, cert: certificate, ca: ca};
-var https = require('https');
-https.createServer(options, app).listen(443);
+/*
+    var fs = require('fs');
+    var privateKey = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/privkey.pem', 'utf8');
+    var certificate = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/cert.pem', 'utf8');
+    var ca = fs.readFileSync('/etc/letsencrypt/live/standardrequests.com/chain.pem', 'utf8');
+    var options = {key: privateKey, cert: certificate, ca: ca};
+    var https = require('https');
+    https.createServer(options, app).listen(443);
+    */
