546d1326698469efec2b00dc326eaaf1cd6a2f21
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-08-12 20:31:18 -0500

    asdf

--- server/userFilters.js
+++ server/userFilters.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
 
 exports.updateFilters = function(req, res) {
+    console.log('UpdateFilters called');
     var myJson = {
         key: "myValue"
     }
     fs.writeFile( "filename.json", JSON.stringify( myJson ), "utf8");
+    var json = require('./filename.json');
+    console.log('Filters is', json);
 }
