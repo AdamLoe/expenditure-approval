bee6a376830fcff87b74268e1df65cf580230199
Adam Loe <aFiddlinCRACKER@gmail.com>
2017-08-12 20:54:28 -0500

    adf

--- server/userFilters.js
+++ server/userFilters.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var knex = require('./knexfile.js');
 
 var callback = function(data) {
     console.log('json filter callback', data)
@@ -6,13 +7,42 @@ var callback = function(data) {
 
 exports.updateFilters = function(req, res) {
     console.log('UpdateFilters called');
-    var myJson = {
-        key: "myValue"
-    }
-    fs.writeFile( "filename.json", JSON.stringify( myJson ), "utf8", callback);
+    updateApprovers();
+    updateRequesters();
 }
 
-exports.getFilters = function() {
-    var json = require('../filename.json');
+var updateApprovers = function() {
+     knex('users').where({
+        type: 'approver',
+        status: 'true'
+    })
+        .then(function(data) {
+            fs.writeFile( "approvers.json", data, "utf8", callback);
+        })
+        .catch(function(err){
+            console.log('Updating Approvers went wrong, this is bad.');
+        })
+}
+
+var updateRequesters = function() {
+    knex('users').where({
+        type: 'requester',
+        status: 'true'
+    })
+        .then(function(data) {
+            fs.writeFile( "requester.json", data, "utf8", callback);
+        })
+        .catch(function(err){
+            console.log('Updating Requesters went wrong, this is bad.');
+        })
+}
+
+exports.getApprovers = function() {
+    var json = require('../approvers.json');
+    return json;
+}
+
+exports.getRequesters = function() {
+    var json = require('../requesters.json');
     return json;
 }
