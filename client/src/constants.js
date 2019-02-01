f598c5ddb349604f4dd56a0bd0f52591c619ecdc
Adam Loe <aFiddlinCRACKER@gmail.com>
2019-02-01 01:55:53 -0600

    Prod and Development client to server url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,11 @@
-exports.url  = "https://adqsv1eqy0.execute-api.us-east-1.amazonaws.com/dev";
-exports.url  = "http://localhost:2002/api";
+
 exports.apiPort = 2002;
-exports.filesUrl = "https://adamloe.com/";
-export let imgUrl = "https://adamloe.com/images/";
+exports.filesUrl = "https://app.adamloe.com/";
+
+let localURL = "http://localhost:2002/api";
+let outsideURL = "https://adqsv1eqy0.execute-api.us-east-1.amazonaws.com/dev";
+export let url = ( process.env.NODE_ENV === "production" ?
+    outsideURL :
+    localURL
+);
+export let imgUrl = "https://app.adamloe.com/images/";
