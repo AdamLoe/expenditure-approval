99ce24cbbe62ef677dabd87b3a5d3f8d7b74a1d9
Adam Loe <aFiddlinCRACKER@gmail.com>
2019-02-01 01:56:07 -0600

    Attribute change

---
+++ client/src/components/CreateRequest/Attribute.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+export default ({
+	keyProp, value, isRequired, onKeyChange, onValueChange, deleteAttribute
+}) => {
+	return (
+		<div className="Attribute">
+			{ isRequired ||
+				<div className="DeleteAttribute">
+					<img
+						onClick={deleteAttribute}
+						src="http://localhost:2002/exit.png"
+					/>
+				</div>
+			}
+			<input type="text"
+				   readOnly={isRequired? "on" : "off"}
+				   onChange={onKeyChange}
+				   value={keyProp}
+			/>
+			<input type="text"
+				   onChange={onValueChange}
+				   value={value}
+			/>
+		</div>
+	);
+};
+
+
+/*
+
+import React from "react";
+
+export default ({
+keyProp, value, isRequired,
+onKeyChange, onValueChange, deleteAttribute
+}) => {
+console.log('RENDER: Attribute');
+
+return (
+<div className="Attribute">
+{ isRequired ||
+<div className="DeleteAttribute">
+<img
+onClick={deleteAttribute}
+src="http://localhost:2002/exit.png"
+/>
+</div>
+}
+<input type="text"
+value={keyProp}
+onChange={onKeyChange}
+/>
+<input type="text"
+value={value}
+onChange={onValueChange}
+/>
+</div>
+);
+};
+
+
+*/
