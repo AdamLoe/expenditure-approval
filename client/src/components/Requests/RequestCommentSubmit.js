6baec7bb10a94f283dcdda09fa3cd99be116254b
Adam Loe <aFiddlinCRACKER@gmail.com>
2019-02-03 07:57:37 -0600

    Client actually submitting comment now

--- client/src/components/Requests/RequestCommentSubmit.js
+++ client/src/components/Requests/RequestCommentSubmit.js
@@ -1,53 +1,62 @@
 import React from "react";
 
-
-
-
+class CreateComment extends React.Component {
+  state = {
+    comment: ""
+  };
+  submit = () => {
+    let { submitComment } = this.props;
+    let { comment } = this.state;
+    submitComment(comment);
+  };
+  onInput = e => {
+    this.setState({
+      comment: e.target.value
+    });
+  };
+  onClick = () => {
+    this.submit();
+  };
+  render() {
+    return (
+      <div className="CreateComment">
+        <textarea onInput={this.onInput} />
+        <button className="Submit" onClick={this.onClick}>
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
 export default ({ comments, submitComment, approveRequest, rejectRequest }) => {
-
-	console.log("Got Comments", comments);
-	return (
-		<div className="RequestRightSide">
-			<div className="CommentContainer">
-				{ comments.map(comm => {
-					let date = comm[0];
-					let message = comm[1];
-					let author = comm[2];
-					return (
-						<div className="Comment">
-							<div className="CommentContent">
-								<div className="Author">
-									{ author }
-								</div>
-								<div className="Message">
-									{ message }
-								</div>
-							</div>
-							<div className="CommentDate">
-								{ date }
-							</div>
-						</div>
-					);
-				}
-				)}
-			</div>
-			<div className="CreateComment">
-				<textarea />
-				<button className="Submit"
-					onClick={submitComment} >
-					Submit
-				</button>
-			</div>
-			<div className="UpdateRequest">
-				<button className="Accept"
-					onClick={approveRequest}>
-						Approve
-				</button>
-				<button className="Reject"
-					onClick={rejectRequest}>
-						Reject
-				</button>
-			</div>
-		</div>
-	);
+  console.log("Got Comments", comments);
+  return (
+    <div className="RequestRightSide">
+      <div className="CommentContainer">
+        {comments.map(comm => {
+          let date = comm[0];
+          let message = comm[1];
+          let author = comm[2];
+          return (
+            <div className="Comment">
+              <div className="CommentContent">
+                <div className="Author">{author}</div>
+                <div className="Message">{message}</div>
+              </div>
+              <div className="CommentDate">{date}</div>
+            </div>
+          );
+        })}
+      </div>
+      <CreateComment submitComment={submitComment} />
+      <div className="UpdateRequest">
+        <button className="Accept" onClick={approveRequest}>
+          Approve
+        </button>
+        <button className="Reject" onClick={rejectRequest}>
+          Reject
+        </button>
+      </div>
+    </div>
+  );
 };
--- client/src/containers/Requests/RequestItem.js
+++ client/src/containers/Requests/RequestItem.js
@@ -1,53 +1,59 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 import { toggleRequest } from "../../actions/index";
-import { submitComment, approveRequest, rejectRequest } from "../../actions/requests";
+import {
+  submitComment,
+  approveRequest,
+  rejectRequest
+} from "../../actions/requests";
 
 import RequestItemRow from "../../components/Requests/RequestItemRow";
 import RequestDetails from "../../components/Requests/RequestDetails";
 import RequestCommentSubmit from "../../components/Requests/RequestCommentSubmit";
 
-
 let RequestItem = ({ ...props }) => (
-	<div className="RequestItem">
-		<RequestItemRow {...props} />
-		{ props.showBig &&
-			<div className="RequestRowMore">
-				<RequestDetails {...props} />
-				<RequestCommentSubmit {...props}/>
-			</div>
-		}
-	</div>
+  <div className="RequestItem">
+    <RequestItemRow {...props} />
+    {props.showBig && (
+      <div className="RequestRowMore">
+        <RequestDetails {...props} />
+        <RequestCommentSubmit {...props} />
+      </div>
+    )}
+  </div>
 );
 
-let mapStateToProps = (state, {id, index}) => {
-	let req = state.requests.array[index];
-	console.log("MAPSTA: RequestItem", id, index, req);
-	return {
-		requestId: req.requestid,
-		updateDate: req.updatedate,
-		createDate: req.createdate,
-		requestName: req.requestname,
-		amount: req.amount,
-		unitName: req.unitname,
-		requesterName: "Johnny",
-		approverName: "April",
-		description: req.description,
-		attributes: req.attributes,
-		showBig: req.showBig,
-		status: req.status,
-		comments: req.comments
-	};
+let mapStateToProps = (state, { id, index }) => {
+  let req = state.requests.array[index];
+  console.log("MAPSTA: RequestItem", id, index, req);
+  return {
+    requestId: req.requestid,
+    updateDate: req.updatedate,
+    createDate: req.createdate,
+    requestName: req.requestname,
+    amount: req.amount,
+    unitName: req.unitname,
+    requesterName: req.requestername,
+    approverName: req.approvername,
+    description: req.description,
+    attributes: req.attributes,
+    showBig: req.showBig,
+    status: req.status,
+    comments: req.comments
+  };
 };
 
-let mapDispatchToProps = (dispatch, {id, index}) => {
-	return {
-		toggleRequest: () => dispatch(toggleRequest(index)),
-		submitComment: () => dispatch(submitComment(id)),
-		approveRequest: () => dispatch(approveRequest(id)),
-		rejectRequest: () => dispatch(rejectRequest(id))
-	};
+let mapDispatchToProps = (dispatch, { id, index }) => {
+  return {
+    toggleRequest: () => dispatch(toggleRequest(index)),
+    submitComment: comment => dispatch(submitComment(id, comment)),
+    approveRequest: () => dispatch(approveRequest(id)),
+    rejectRequest: () => dispatch(rejectRequest(id))
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(RequestItem);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RequestItem);
