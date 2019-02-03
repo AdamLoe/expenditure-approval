3224bccdaa4a8ca7f42d3bc5f0b6337411cbbf3a
Adam Loe <aFiddlinCRACKER@gmail.com>
2019-02-03 07:45:53 -0600

    fixed lambda issue

--- server/auth/checkLogin.js
+++ server/auth/checkLogin.js
@@ -1,40 +1,41 @@
 let knex = require("../helpers/knexfile.js");
 
-
 module.exports = (event, callback) => {
-	return new Promise( resolve => {
+  return new Promise(resolve => {
+    let { body } = event;
+    let username = body.usr;
+    let password = body.pas;
 
-		knex("users")
-			.where("username", event.body.usr)
-			.where("status", true)
-			.first()
-			.then(function(user) {
-				console.log(user);
-				if (!user) {
-					callback(null, {
-						"statusCode": 403,
-						"body": "{\"message\":\"Username doesnt exist or deleted\"}"
-					});
-				} else if (user.password === event.body.pas){
-					user.password = "null";
-					resolve(user);
-				} else {
-					callback(null,  {
-						"statusCode": 403,
-						"body": "{\"message\":\"Password not right\"}"
-					});
-				}
-			})
-			.catch(function(err) {
-				console.log("Database login fail", err);
-				callback(null, {
-					"statusCode": 500,
-					"body": JSON.stringify({
-						message: "Database login fail",
-						err: err
-					})
-				});
-			});
-
-	});
+    knex("users")
+      .where("username", username)
+      .where("status", true)
+      .first()
+      .then(function(user) {
+        console.log(user);
+        if (!user) {
+          callback(null, {
+            statusCode: 403,
+            body: '{"message":"Username doesnt exist or deleted"}'
+          });
+        } else if (user.password === password) {
+          user.password = "null";
+          resolve(user);
+        } else {
+          callback(null, {
+            statusCode: 403,
+            body: '{"message":"Password not right"}'
+          });
+        }
+      })
+      .catch(function(err) {
+        console.log("Database login fail", err);
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: "Database login fail",
+            err: err
+          })
+        });
+      });
+  });
 };
